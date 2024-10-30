589afc8a2522dfbbe4ab8f7ae3574a9ea06ec509
Ionut Balutoiu <ionut@balutoiu.com>
2024-10-31 01:56:22 +0200

    Add comments to internal/mocks/http_client.go

--- internal/mocks/http_client.go
+++ internal/mocks/http_client.go
@@ -9,16 +9,19 @@ import (
 	"golang.org/x/net/html"
 )
 
+// MockedHTTPClient is a mocked implementation of the HTTPClient interface from html_parser package.
 type MockedHTTPClient struct {
 	testDataDir string
 }
 
+// NewMockedHTTPClient creates a new instance of MockedHTTPClient.
 func NewMockedHTTPClient(testDataDir string) MockedHTTPClient {
 	return MockedHTTPClient{
 		testDataDir: testDataDir,
 	}
 }
 
+// LoadPage loads a page from the test data directory. It returns the parsed HTML node of the page.
 func (m MockedHTTPClient) LoadPage(url string) (*html.Node, error) {
 	parsedUrl, err := net_url.ParseRequestURI(url)
 	if err != nil {
